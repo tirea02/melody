04eb7452056b31225b95a6ca296dd95790495a3c
msjin311 <136568295+msjin311@users.noreply.github.com>
2023-08-01 15:26:02 +0900

    edit playlist servlet
    Former-commit-id: f492dd6dbbaae2225b2566feb20df320d3a6a473

--- src/main/java/com/melody/controller/playlist/PlaylistDisplayServlet.java
+++ src/main/java/com/melody/controller/playlist/PlaylistDisplayServlet.java
@@ -1,23 +1,28 @@
-package com.melody.controller.playlist;
+package com.melody.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.melody.dao.PlayListDAOImpl;
+import com.melody.model.PlayList;
+
 /**
- * Servlet implementation class PlaylistDisplay
+ * Servlet implementation class PlayListDisplay
  */
-@WebServlet("/PlaylistDisplay")
-public class PlaylistDisplayServlet extends HttpServlet {
+@WebServlet("/PlayListDisplay")
+public class PlayListDisplay extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PlaylistDisplayServlet() {
+    public PlayListDisplay() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,6 +33,12 @@ public class PlaylistDisplayServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PlayListDAOImpl PlayListDAO = new PlayListDAOImpl();
+		List<PlayList> playlists = new PlayListDAOImpl().showPlayList();
+		request.setAttribute("playlists", playlists);
+		
+		
 	}
 
 	/**
--- src/main/java/com/melody/controller/playlist/PlaylistInsertServelt.java
+++ src/main/java/com/melody/controller/playlist/PlaylistInsertServelt.java
@@ -1,23 +1,33 @@
-package com.melody.controller.playlist;
+package com.melody.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.melody.dao.PlayListDAOImpl;
+import com.melody.util.DatabaseConfig;
+
 /**
- * Servlet implementation class PlaylistInsert
+ * Servlet implementation class PlayListServlet
  */
-@WebServlet("/PlaylistInsert")
-public class PlaylistInsertServelt extends HttpServlet {
+@WebServlet("/createPlayList")
+public class PlayListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PlaylistInsertServelt() {
+    public PlayListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,9 +43,24 @@ public class PlaylistInsertServelt extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		response.setContentType("text/html; charset=UTF-8");
+	    
+	    try (Connection connection = DatabaseConfig.getConnection()){
+	    	PlayListDAOImpl PlayListDAO = new PlayListDAOImpl();
+	    	PlayListDAO.createPlayList();
+	    } catch(SQLException e) {
+
+	    }
+	    
+	    response.sendRedirect("pages/playList.jsp");
+	    //양식에 맞춰서 다시분리
+	    
+	    
+
+		
+		
+		
 	}
 
 }
