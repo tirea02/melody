ab8d69e4bc8392276a99c7163e129578d23837b2
tirea02 <tirea02@gmail.com>
2023-08-04 14:18:00 +0900

    페이지네이션과 장르 tab 2개 추가
    페이지네이션과 장르 tab 2개 추가

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,21 +1,37 @@
-// main.js
 $(document).ready(function() {
     // Array of genre IDs to fetch data for
-    var genreIds = [1, 2, 3, 4, 5];
+    var genreIds = [1, 2, 3, 4, 5, 6];
+    var currentPage = {};
+    var totalPages = {};
+
+    // $('.genre-tab1').show();
+    //
+    // $('.genre-btn-tab a').click(function() {
+    //     var genreId = $(this).attr('data-genre-id');
+    //
+    //     // Show the corresponding genre-tab and hide others
+    //     $('.genre-tab').hide();
+    //     $('.genre-tab' + genreId).show();
+    //
+    //     // Add 'active' class to the clicked tab and remove from siblings
+    //     $(this).addClass('active').siblings().removeClass('active');
+    // });
 
     // Function to make the AJAX call to retrieve song data for each genre ID
-    function fetchGenreSongs(genreId) {
-
-        var ajaxUrl = contextPath+"/genreSongServlet";
+    function fetchGenreSongs(genreId, pageNumber) {
+        currentPage[genreId] = pageNumber;
 
         $.ajax({
-            url: ajaxUrl,
-            data: { genreId: genreId },
+            url: contextPath + "/genreSongServlet",
+            data: {
+                genreId: genreId,
+                pageNumber: pageNumber
+            },
             dataType: "json",
             success: function(data) {
-                console.log("Genre ID: " + genreId);
-                console.log(data); // For testing purposes, log the data to the console
-                renderGenreSongs(genreId, data);
+                renderGenreSongs(genreId, data.songs); // Use data.songs property from the response
+                totalPages[genreId] = Math.ceil(data.totalSongs / 10); // Calculate total pages using totalSongs property
+                updatePagination(genreId, totalPages[genreId]); // Pass totalPages[genreId] to the updatePagination function
             },
             error: function(xhr, status, error) {
                 console.error("AJAX request error for Genre ID " + genreId + ":", status, error);
@@ -25,9 +41,9 @@ $(document).ready(function() {
 
     // Loop through each genre ID and make the AJAX call
     for (var i = 0; i < genreIds.length; i++) {
-        fetchGenreSongs(genreIds[i]);
+        console.log(`genreIds : ${genreIds}`);
+        fetchGenreSongs(genreIds[i], 1); // Fetch songs for page 1
     }
-
 
     // Function to render the song data for a specific genre ID
     function renderGenreSongs(genreId, songs) {
@@ -54,4 +70,27 @@ $(document).ready(function() {
             table.append(row);
         });
     }
+
+    // Function to update the pagination buttons
+    function updatePagination(genreId, totalPages) {
+        var paginationDiv = $(".genre-tab" + genreId + " .table-btn");
+        paginationDiv.empty();
+        for (var page = 1; page <= totalPages; page++) {
+            var pageLink = $("<a>").text(page).attr("href", "#").attr("data-genre-id", genreId).attr("data-page", page);
+            if (currentPage[genreId] === page) {
+                pageLink.addClass("pagination");
+                pageLink.addClass("active");
+            }
+            paginationDiv.append(pageLink);
+        }
+    }
+
+    // Event delegation for click event on pagination links
+    $(".table-btn").on("click", "a", function() {
+        console.log("loadPage called");
+        var genreId = $(this).attr("data-genre-id");
+        var pageNumber = $(this).attr("data-page");
+        fetchGenreSongs(genreId, pageNumber);
+        return false;
+    });
 });
