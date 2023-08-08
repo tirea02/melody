d9252802565bfee1fcecaff3ca6181f126bfe494
msjin311 <136568295+msjin311@users.noreply.github.com>
2023-08-08 14:48:22 +0900

    플레이리스트 생성 추가
    플레이리스트 생성 추가

--- src/main/java/com/melody/controller/AddPlaylistServlet.java
+++ src/main/java/com/melody/controller/AddPlaylistServlet.java
@@ -2,12 +2,16 @@ package com.melody.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.melody.dao.PlaylistDAO;
 import com.melody.model.Playlist;
@@ -19,41 +23,38 @@ import com.melody.model.UserAccount;
 @WebServlet("/AddPlaylistServlet")
 public class AddPlaylistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddPlaylistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final Logger logger = LoggerFactory.getLogger(AddToPlaylistServlet.class);
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			UserAccount userAccount = (UserAccount) request.getSession().getAttribute("userAccount");
 			long useraccountId = userAccount.getUserAccountId();
 			
 			String playlistName = request.getParameter("playlistname");
 			String playlistDescription = request.getParameter("playlistdescription");
+			Date date = new java.util.Date();
 			String playlistHasgtags = request.getParameter("playlisthashtags");
 			
-					
-			Playlist playlist = new Playlist();
+			logger.debug(String.valueOf(useraccountId));
+			logger.debug(playlistName);
+			logger.debug(playlistDescription);
+			logger.debug(String.valueOf(date));
+			logger.debug(playlistHasgtags);
+			
+			Playlist playlist = new Playlist( useraccountId , playlistName, playlistDescription, date, playlistHasgtags );
 			playlist.setPlaylistName(playlistName);
 			playlist.setUserAccountId(useraccountId);
 			playlist.setDescription(playlistDescription);
-			playlist.setPlaylistHashtags(playlistHasgtags);
+			playlist.setCreatedDate(date);
 			
 			
 			PlaylistDAO PlaylistDAO = new PlaylistDAO();
 			PlaylistDAO.addPlaylist(playlist);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		request.getRequestDispatcher("/pages/main-login.jsp").forward(request, response);
 	}
 
 }
