577ad19b96ccc514f714922b182060c5946ad7df
tirea02 <tirea02@gmail.com>
2023-08-09 12:07:18 +0900

    delete playlist redirect 및 data fetch 추가

--- src/main/java/com/melody/controller/DeletePlaylistServlet.java
+++ src/main/java/com/melody/controller/DeletePlaylistServlet.java
@@ -2,6 +2,7 @@ package com.melody.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.melody.model.CustomPlaylist;
+import com.melody.model.UserAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +27,12 @@ public class DeletePlaylistServlet extends HttpServlet {
 	private static final Logger logger = LoggerFactory.getLogger(AddToPlaylistServlet.class);
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+
+		UserAccount userAccount = (UserAccount) request.getSession().getAttribute("userAccount");
+		long userAccountId = userAccount.getUserAccountId();
+
+
 		long playlistId = Long.parseLong(request.getParameter("playlistId"));
 		logger.debug(String.valueOf(playlistId));
 		
@@ -32,8 +42,22 @@ public class DeletePlaylistServlet extends HttpServlet {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+		assert userAccount != null;
+		List<CustomPlaylist> customPlaylists = playlistDAO.getAllPlaylistsForUser(userAccount.getUserAccountId());
+		logger.debug("customPlaylists : {}" , customPlaylists);
+
+		Gson gson = new Gson();
+		String jsonCustomPlaylists = gson.toJson(customPlaylists);
+
+		request.getSession().setAttribute("customPlaylists", customPlaylists);
+		request.getSession().setAttribute("jsonCustomPlaylists", jsonCustomPlaylists);
+
+		response.sendRedirect(request.getContextPath() + "/pages/main-login.jsp");
 		
-	
+
+
+
 	}
 
 
